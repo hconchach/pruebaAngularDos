6fdb7c2f6b56983a24e3ee88f18eedb8779eb9a7
unknown <hectorconcha@gmail.com>
2020-02-29 18:11:36 -0300

    Cuarto Comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pruebaAngularDos';
+  public title = 'Shellium';
+  public description = 'Curso de Introducción';
 }
