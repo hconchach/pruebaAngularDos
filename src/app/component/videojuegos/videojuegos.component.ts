b8012b0d8e2c33e4023330e2df38568fabd28818
unknown <hectorconcha@gmail.com>
2020-02-29 22:16:06 -0300

    Mi Primer Componente

--- src/app/component/videojuegos/videojuegos.component.ts
+++ src/app/component/videojuegos/videojuegos.component.ts
@@ -3,11 +3,16 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'videojuegoComponent',
     template: `
-        <h2> Componente de {{nombre}} <h2>
+        <h2> Componente de {{nombre}} </h2>
+        <h3 *ngIf="mostrarJuego != true"> Yo adoro a la {{amorMio}}</h3>
+        <h3 *ngIf="mostrarJuego"> El mejor juego es {{mejorVideojuego}}</h3>
     `
 })
 
 export class VideojuegosComponent{
 
     public nombre = 'Videojuego';
+    public amorMio = 'Dani';
+    public mejorVideojuego = 'Starcraft';
+    public mostrarJuego = false;
 }
